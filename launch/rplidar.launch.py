1fd24a6b1e4b5ce4cb0cd94eecaf79b181f747f4
smolalpaca <aaaimanasyraf@gmail.com>
2023-06-16 11:04:02 +0800

    Applying ros2_control on Simulation

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,20 +1,20 @@
-# import os
-# from launch import LaunchDescription
-# from launch_ros.actions import Node
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-# def generate_launch_description():
+def generate_launch_description():
 
-#     return LaunchDescription([
+    return LaunchDescription([
 
-#         Node(
-#             package='rplidar_ros',
-#             executable='rplidar_composition',
-#             output='screen',
-#             parameters=[{
-#                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-#                 'frame_id': 'laser_frame',
-#                 'angle_compensate': True,
-#                 'scan_mode': 'Standard'
-#             }]
-#         )
-#     ])
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
