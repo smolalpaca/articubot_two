89cbf3e738397953329c5c8f5cf27a377e368c0a
smolalpaca <aaaimanasyraf@gmail.com>
2023-06-28 01:44:47 +0800

    Housekeeping

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -42,7 +42,7 @@ def generate_launch_description():
                     controller_params_file]
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    delayed_controller_manager = TimerAction(period=5.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
